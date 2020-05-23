a08f9058248472186da20175974a5598cdd4f77b
cam2149 <cam2149@gmail.com>
2020-05-22 19:40:41 -0500

    Este es mi primer commit - E

---
+++ ejemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	
+printf("************************************************\n");
+printf("Práctica: configurar y Usar Github en linux RHEL\n");
+printf("************************************************\n");
+printf("Carlos Alberto Martinez Ramirez");
+	return 0;
+}
